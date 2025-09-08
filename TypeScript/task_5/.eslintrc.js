ad4f58304114b22a89188c842b44b8d91d4651b9
root <10679@holbertonstudents.com>
2025-09-08 11:45:10 +0000

    .eslintrc.js

--- TypeScript/task_5/.eslintrc.js
+++ TypeScript/task_5/.eslintrc.js
@@ -4,9 +4,10 @@ module.exports =  {
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   parserOptions:  {
-    ecmaVersion:  2018,
+    ecmaVersion:  2020,
     sourceType:  'module',
+    project: './tsconfig.json'
   },
   rules:  {
   },
-};
+}
